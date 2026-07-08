18e600aaefb7a4f5d2abc4eca0483076806704a6
Piotr P. Karwasz <pkarwasz-github@apache.org>
2026-07-08 23:24:21 +0200

    fix: remove copyright notice line (ppkarwasz)

--- src/test/java/org/apache/commons/xml/XIncludeTest.java
+++ src/test/java/org/apache/commons/xml/XIncludeTest.java
@@ -1,6 +1,5 @@
 /*
  * SPDX-FileCopyrightText: 2026 Ta Thien
- * SPDX-FileCopyrightText: 2026 Piotr P. Karwasz <piotr@github.copernik.eu>
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with

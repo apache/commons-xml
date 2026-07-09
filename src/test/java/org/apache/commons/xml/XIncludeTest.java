ee98041a9badfcd4b85b6702e81a4d02df669d29
Piotr P. Karwasz <piotr@github.copernik.eu>
2026-07-09 07:02:53 +0200

    Remove copyright attribution line (thientd)
    https://github.com/apache/commons-xml/pull/21#discussion_r3548065556
    
    Co-authored-by: Ta Duc Thien <thienbk89@gmail.com>

--- src/test/java/org/apache/commons/xml/XIncludeTest.java
+++ src/test/java/org/apache/commons/xml/XIncludeTest.java
@@ -1,5 +1,4 @@
 /*
- * SPDX-FileCopyrightText: 2026 Ta Thien
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
